3cb5b377e0017f6c6c6df1685dfbf41364b1727b
walther <waltheroliveirapires@gmail.com>
2024-09-25 01:51:12 -0300

    dtos e UserRepository

--- Data/IUserRepository.cs
+++ Data/IUserRepository.cs
@@ -1,3 +1,5 @@
+using DotnetAPI.Models;
+
 namespace DotnetAPI.Data
 {
     public interface IUserRepository
@@ -5,5 +7,9 @@ namespace DotnetAPI.Data
         public bool SaveChanges();
         public void AddEntity<T>(T entityAdd);
         public void RemoveEntity<T>(T entityToRemove);
+        public IEnumerable<User> GetUsers();
+        public User GetSingleUser(int userId);
+        public UserSalary GetSingleUserSalary(int userId);
+        public UserJobInfo GetSingleUserJobInfo(int userId);
     }
 }
